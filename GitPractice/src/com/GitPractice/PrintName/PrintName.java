d4cfe05b5f183239e5af32a9ad3edc5dfd7c8907
b <b@DESKTOP-A28BAJQ>
2019-12-08 20:17:27 -0500

    my changes 2

--- GitPractice/src/com/GitPractice/PrintName/PrintName.java
+++ GitPractice/src/com/GitPractice/PrintName/PrintName.java
@@ -5,7 +5,7 @@ public class PrintName {
 	public static void main(String[] args) {
 		System.out.println("My name is Bilal");
 		
-		System.out.println("my Another println");
+		System.out.println("my Another println"); // any new change but no commit will come with error mark which says commmit is pending. so i need to commit it.. new changees will be unstaged.. not need to update again
 
 	}
 	
